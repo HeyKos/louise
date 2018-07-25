f0beb7233dd47392f338a2fdf2ca582b3111b337
mkoser-andculture <mkoser@andculture.com>
2018-07-25 09:44:51 -0400

    Added node modules, and initialized basic mongo config.

---
+++ index.js
@@ -0,0 +1,11 @@
+var SlackBot = require('slackbots'); // import slackbot library
+var mongoose = require('mongoose');  // import mongoose library for accessing MongoDB
+
+var http = require('http');
+http.createServer((req, res) => {}).listen(1337, '127.0.0.1');
+
+/* Create MongoDB Connection */
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/louise', { useMongoClient: true, promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
