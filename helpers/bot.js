b6ca35b0191a05e3acb6f864506172f6194ffb5b
mkoser-andculture <mkoser@andculture.com>
2018-07-28 09:51:54 -0400

    Played around with configuring event api

--- helpers/bot.js
+++ helpers/bot.js
@@ -1,36 +1,30 @@
-var SlackBot  = require('slackbots');
-var mongoose  = require('mongoose');
-var Developer = require('../models/Developer.js');
+const { IncomingWebhook, WebClient } = require('@slack/client');
+const dotenv = require('dotenv').config();
 
-var bot = new SlackBot({
-    token: process.env.BOT_API_KEY,
-    name: 'SlackBot'
-});
+console.log('Getting started with Slack Developer Kit for Node.js');
+console.log("Webhook URL", process.env.SLACK_WEBHOOK_URL)
+console.log("Access Token", process.env.SLACK_TOKEN)
 
-exports.run = () => {
-    bot.on('start', onStart);
-    bot.on('message', onMessage);
+const web = new WebClient(process.env.SLACK_TOKEN);
+const timeNotification = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+const currentTime = new Date().toTimeString();
+
+timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
+  if (error) {
+    return console.error(error);
   }
-
-var onStart = () => {
-  console.log('Bot started');
-}
-
-var onMessage = (message) => {
-    console.log("We got a message: " + message);
-    // users = [];
-    // channels = [];
-    // var botUsers = bot.getUsers();
-    // users = botUsers._value.members;
-    // var botChannels = bot.getChannels();
-    // channels = botChannels._value.channels;
-  
-    // if (message.type === 'message' && Boolean(message.text)) {
-    //     var channel = channels.find(channel => channel.id === message.channel);
-    //     var usr = users.find(user => user.id === message.user);
-  
-    //     if(usr.name !== 'louise') {
-    //         bot.postMessageToChannel(channel.name, 'You just said: "' + message + '".' , { as_user: true });    
-    //     }
-    // }
-}
+  console.log('Notification sent');
+  console.log('Waiting a few seconds for search indexes to update...');
+  setTimeout(() => {
+    console.log('Calling search.messages');
+    web.search.messages({ query: currentTime })
+      .then(resp => {
+        if (resp.messages.total > 0) {
+          console.log('First match:', resp.messages.matches[0]);
+        } else {
+          console.log('No matches found');
+        }
+      })
+      .catch(console.error)
+  }, 12000);
+});
--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 var dotenv   = require('dotenv').config();
-var slackbot = require('./helpers/bot'); // import slackbot library
+// var slackbot = require('./helpers/bot'); // import slackbot library
 var mongoose = require('mongoose');  // import mongoose library for accessing MongoDB
 
-var http = require('http');
-http.createServer((req, res) => {}).listen(1337, '127.0.0.1');
+// BEGIN
+
+// Initialize using verification token from environment variables
+const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+// Start a basic HTTP server
+slackEvents.start(port).then(() => {
+  console.log(`server listening on port ${port}`);
+});
+
+// END
+
+// BEGIN EXPRESS SLACK CONFIG
+// const http = require('http');
+
+// // Initialize using verification token from environment variables
+// const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+// const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+// const port = process.env.PORT || 3000;
+
+// // Initialize an Express application
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+
+// // You must use a body parser for JSON before mounting the adapter
+// app.use(bodyParser.json());
+
+// // Mount the event handler on a route
+// // NOTE: you must mount to a path that matches the Request URL that was configured earlier
+// app.use('/slack/events', slackEvents.expressMiddleware());
+
+// // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+// slackEvents.on('message', (event)=> {
+//   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+// });
+
+// // Handle errors (see `errorCodes` export)
+// slackEvents.on('error', console.error);
+
+// // Start the express application
+// http.createServer(app).listen(port, () => {
+//   console.log(`server listening on port ${port}`);
+// });
+// END EXPRESS SLACK CONFIG
 
 /* Create MongoDB Connection */
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/louise', { useMongoClient: true, promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection successful'))
   .catch((err) => console.error(err));
-
-slackbot.run();
